6cd3f29fd2260e7840c15a4ffbb8352a77f5fbfd
vairome <byron.murillo813@est.uca.edu.ni>
2022-07-06 19:33:21 -0600

    format archives

--- routes.tf
+++ routes.tf
@@ -11,10 +11,10 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   route {
-      cidr_block                 = "0.0.0.0/0"
-      nat_gateway_id             = aws_nat_gateway.nat.id
-      
-    }
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+
+  }
   tags = {
     Name = "private"
   }
