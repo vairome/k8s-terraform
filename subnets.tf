3b083cd8c83bfbad12fc02fa9622cd6ecc9106c5
Byron Murillo <bmurillo@gbm.net>
2022-07-07 12:33:56 -0600

    Refactoring some resources

--- subnets.tf
+++ subnets.tf
@@ -40,25 +40,3 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
 
 }
 
-resource "aws_security_group" "rds_sg" {
-  name = "postgres-rds-sg-${terraform.workspace}"
-
-  description = "RDS security group for Postgres RDS instances"
-  vpc_id      = aws_vpc.main.id
-
-  # Inbound traffic
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all outbound traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
